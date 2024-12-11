a90d724efe67a5c6d56c77270a365fc03ffd3f01
Aakshi Gulati <122432167+Aakshigulati098@users.noreply.github.com>
2024-12-11 16:54:06 +0530

    Update DoctorAppointment.jsx

--- frontend/src/pages/Doctor/DoctorAppointment.jsx
+++ frontend/src/pages/Doctor/DoctorAppointment.jsx
@@ -86,7 +86,7 @@ const DoctorAppointments = () => {
                         </span>
                       ) : item.isCompleted ? (
                         <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
-                          Completed
+                          Accepted
                         </span>
                       ) : (
                         <div className="flex gap-2">
